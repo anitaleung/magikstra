bff2c5f51cf36f933d4840ffc0508a7a226e3abd
Anita Leung <anita.wk.leung@gmail.com>
2014-03-07 17:34:59 -0800

    Adding sections for landing page

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'header.php'; ?>
+
+<section class="landing">
+	<a href="#landing"></a>
+	<?php include 'landing.php'; ?>
+</section>
+
+<section class="nav">
+	<?php include 'nav.php'; ?>
+</section>
+
+<section class="about">
+	<a href="#about"></a>
+	<?php include 'about.php'; ?>
+</section>
+
+<section class="team">
+	<a href="team"></a>
+	<?php include 'team.php'; ?>
+</section>
+
+<section class="share">
+	<?php include 'share.php'; ?>
+</section>
+
+<section class="notify">
+	<?php include 'notify.php'; ?>
+</section>
+
+<?php include 'footer.php'; ?>
+
