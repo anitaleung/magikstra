d5d7f86c231130048f4ab0c408866e666cc2df98
Anita Leung <anita.wk.leung@gmail.com>
2014-03-08 19:59:15 -0800

    Updated files

--- header.php
+++ header.php
@@ -6,14 +6,18 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
+        <title>Magikstra - A networking site for students to connect with mentors and university faculty members.</title>
+        <meta name="description" content="A networking site for students to connect with mentors and university faculty members.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Typography -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
         <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/foundation.min.css">
+        <link rel="stylesheet" href="css/style.css">
         <!-- <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
     </head>
     <body>
---
+++ landing.php
@@ -0,0 +1,15 @@
+<div class="row text-center">
+
+	<img src="img/logo.png" alt="Magikstra Logo" id="site-logo">
+
+	<p id="site-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ea, aperiam, necessitatibus tenetur tempore maxime molestiae dolorum et nobis sequi mollitia deleniti nesciunt ullam minima laborum. Maiores, similique, nemo distinctio dicta quasi placeat amet.</p>
+
+	<div class="columns large-4 push-3">
+		<input type="email" name="email" placeholder="Email" class="email">
+	</div>
+
+	<div class="columns large-1 pull-4">
+		<input type="submit" value="Notify Me" class="notify">
+	</div>
+
+</div>
