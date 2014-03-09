b791a58478b60507ae0de6059e9026f9de7caae5
Anita Leung <anita.wk.leung@gmail.com>
2014-03-09 16:28:57 -0700

    Added team section

---
+++ team.php
@@ -0,0 +1,76 @@
+<div class="row">
+
+	<h2 id="team-title" class="section-title">Meet the Team</h2>
+
+	<ul id="exec" class="large-block-grid-5 medium-block-grid-3 small-block-grid-2">
+
+		<li>
+			<div class="exec-img"></div>
+			<span class="name">First Last</span>
+			<span class="role">Role</span>
+		</li>
+		<li>
+			<div class="exec-img"></div>
+			<span class="name">First Last</span>
+			<span class="role">Role</span>
+
+		</li>
+		<li>
+			<div class="exec-img"></div>
+			<span class="name">First Last</span>
+			<span class="role">Role</span>
+
+		</li>
+		<li>
+			<div class="exec-img"></div>
+			<span class="name">First Last</span>
+			<span class="role">Role</span>
+
+		</li>
+		<li>
+			<div class="exec-img"></div>
+			<span class="name">First Last</span>
+			<span class="role">Role</span>
+
+		</li>
+
+	</ul>
+
+	<div id="sub-teams">
+
+		<ul class="columns large-3">
+			<h3>Front-end Development</h3>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+		</ul>
+
+		<ul class="columns large-3">
+			<h3>Back-end Development</h3>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+		</ul>
+
+		<ul class="columns large-3">
+			<h3>Marketing &amp; Branding</h3>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+		</ul>
+
+		<ul class="columns large-3">
+			<h3>Advisory Board</h3>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+			<li><span class="name">First Last</span><span class="role">Role</span></li>
+		</ul>
+
+	</div>
+	
+
+
+</div>
