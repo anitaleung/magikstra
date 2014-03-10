5e4cfbe6bafa28d407af8b00b53316de6f05ba02
Anita Leung <anita.wk.leung@gmail.com>
2014-03-09 17:20:46 -0700

    Added share section

---
+++ share.php
@@ -0,0 +1,10 @@
+<div class="row text-center">
+	
+	<ul id="share">
+		<li id="tumblr-icon"><a href=""></a></li>
+		<li id="twitter-icon"><a href=""></a></li>
+		<li id="facebook-icon"><a href=""></a></li>
+		<li id="email-icon"><a href=""></a></li>
+	</ul>
+
+</div>
