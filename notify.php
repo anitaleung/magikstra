1b008dc602eb045469ced7d53f1f39c7c5aa11fb
Anita Leung <anita.wk.leung@gmail.com>
2014-03-09 17:21:14 -0700

    Add notify section

---
+++ notify.php
@@ -0,0 +1,17 @@
+<div class="row text-center">
+
+	<span>Sign up for a notification now!</span>
+
+	<form>
+
+		<div class="columns large-4 push-3">
+			<input type="email" name="email" placeholder="Email" class="email" autocomplete="off">
+		</div>
+
+		<div class="columns large-1 pull-4">
+			<input type="submit" value="Notify Me" class="notify">
+		</div>
+
+	</form>
+
+</div>
