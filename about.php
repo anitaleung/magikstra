eb050b8a52e31e639bedb9821d181ba6e0c1bb41
heather huynh <amphy@uga.edu>
2014-03-14 10:46:21 -0400

    Updated style for about page
    Should all be done now.

--- about.php
+++ about.php
@@ -1,36 +1,36 @@
 <div class="row">
 	<div class="large-6 columns">
-	<h1>About Us</h1>
+	<center><h1 class="about"><b>ABOUT US</b></h1></center>
 	</div>
 </div>
 <br />
 <div class="row">
 	<div class="large-6 columns">
-	<h2>Design</h2><p></p>
-	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	<img src="/img/circle1.gif">
 	</div>
 	<div class="large-6 columns">
-	[circle image here]
-	</div>
-</div>
-<br />
-<div class="row">
-	<div class="large-6 columns">
-	[circle image here]
-	</div>
-	<div class="large-6 columns">
-	<h2>Usability</h2><p></p>
+	<h2 class="first">Design</h2><p></p>
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	</div>
 </div>
 <br />
 <div class="row">
 	<div class="large-6 columns">
-	<h2>Experience<h2><p></p>
+	<h2 class="second">Usability</h2><p></p>
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	</div>
 	<div class="large-6 columns">
-	[circle image here]
+	<img src="/img/circle2.gif">
+	</div>
+</div>
+<br />
+<div class="row">
+	<div class="large-6 columns">
+	<img src="/img/circle3.gif">
+	</div>
+	<div class="large-6 columns">
+	<h2 class="third">Experience<h2><p></p>
+	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	</div>
 </div>
 
