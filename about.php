cab3e95aa2d4deb0bd3822fcf44c326a50b721b8
heather huynh <amphy@uga.edu>
2014-03-14 10:28:50 -0400

    all except colors for text headings and circles
    will add circles in little bit

--- about.php
+++ about.php
@@ -1,17 +1,38 @@
-<div class="row text-center">
-
-
-	<h3>About Us</h3>
-	<p></p>
-	<h1>Design</h1><p></p>
+<div class="row">
+	<div class="large-6 columns">
+	<h1>About Us</h1>
+	</div>
+</div>
+<br />
+<div class="row">
+	<div class="large-6 columns">
+	<h2>Design</h2><p></p>
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	<p></p>
-	<h1>Usability</h1><p></p>
+	</div>
+	<div class="large-6 columns">
+	[circle image here]
+	</div>
+</div>
+<br />
+<div class="row">
+	<div class="large-6 columns">
+	[circle image here]
+	</div>
+	<div class="large-6 columns">
+	<h2>Usability</h2><p></p>
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	<p></p>
-	<h1>Experience<h1><p></p>
+	</div>
+</div>
+<br />
+<div class="row">
+	<div class="large-6 columns">
+	<h2>Experience<h2><p></p>
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	<p></p>
+	</div>
+	<div class="large-6 columns">
+	[circle image here]
+	</div>
+</div>
 
 
 </div>
