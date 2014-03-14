6b1785475dfe366f74925ecf3812ecb89d93492e
Anita Leung <anita.wk.leung@gmail.com>
2014-03-13 18:55:23 -0700

    Updated landing page

--- landing.php
+++ landing.php
@@ -4,12 +4,16 @@
 
 	<p id="site-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, ea, aperiam, necessitatibus tenetur tempore maxime molestiae dolorum et nobis sequi mollitia deleniti nesciunt ullam minima laborum. Maiores, similique, nemo distinctio dicta quasi placeat amet.</p>
 
-	<div class="columns large-4 push-3">
-		<input type="email" name="email" placeholder="Email" class="email">
-	</div>
+	<form>
 
-	<div class="columns large-1 pull-4">
-		<input type="submit" value="Notify Me" class="notify">
-	</div>
+		<div class="columns large-4 push-3">
+			<input type="email" name="email" placeholder="Email" class="email" autocomplete="off">
+		</div>
+
+		<div class="columns large-1 pull-4">
+			<input type="submit" value="Notify Me" class="notify">
+		</div>
+
+	</form>
 
 </div>
