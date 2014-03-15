c865e70e7dc6d27f35fdec111c77995cfd93e391
Anita Leung <anita.wk.leung@gmail.com>
2014-03-15 13:35:55 -0700

    Update about

--- about.php
+++ about.php
@@ -1,45 +1,48 @@
 <div class="row">
-	<div class="large-12 columns text-center">
-	<h1 id="about" class="section-title">About Us</h1>
-	</div>
-</div>
-<br />
-<div class="row">
-	<div class="large-4 columns">
-	<img src="img/circle1.gif">
-	</div>
-	<div id="about-words" class="large-8 columns">
-	<h2 class="first">Design</h2>
-	<p id="about-description">
-	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	</p>
-	</div>
-</div>
-<br />
-<div class="row">
-	<div id="about-words" class="large-8 columns">
-	<h2 class="second">Usability</h2>
-	<p id="about-description">
-	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	</p>
-	</div>
-	<div class="large-4 columns">
-	<img src="img/circle2.gif">
-	</div>
-</div>
-<br />
-<div class="row">
-	<div class="large-4 columns">
-	<img src="img/circle3.gif">
-	</div>
-	<div id="about-words" class="large-8 columns">
-	<h2 class="third">Experience</h2>
-	<p id="about-description">
-	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	</p>
-	</div>
-</div>
 
+	<h1 id="about-title" class="text-center section-title">About Us</h1>
+
+	<div class="row equalize">
+		<div class="large-4 columns text-center">
+			<img src="img/circle1.png">
+		</div>
+		<div class="large-8 columns to">
+			<div class="ti">
+				<h2 id="first">Design</h2>
+				<p class="about-description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row equalize">
+		<div class="large-8 columns to">
+			<div class="ti">
+				<h2 id="second">Usability</h2>
+				<p class="about-description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+		<div class="large-4 columns text-center">
+			<img src="img/circle2.png">
+		</div>
+	</div>
+
+	<div class="row equalize">
+		<div class="large-4 columns text-center">
+			<img src="img/circle3.png">
+		</div>
+		<div class="large-8 columns to">
+			<div class="ti">
+				<h2 id="third">Experience</h2>
+				<p class="about-description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	</div>
 
 </div>
 
