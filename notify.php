594e43533b46e347b0199ecdcb2277cdcfaa5bef
Anita Leung <anita.wk.leung@gmail.com>
2014-03-14 22:29:00 -0700

    Updated notify form for smaller screen resolutions

--- notify.php
+++ notify.php
@@ -2,16 +2,9 @@
 
 	<span>Sign up for a notification now!</span>
 
-	<form>
-
-		<div class="columns large-4 push-3">
-			<input type="email" name="email" placeholder="Email" class="email" autocomplete="off">
-		</div>
-
-		<div class="columns large-1 pull-4">
-			<input type="submit" value="Notify Me" class="notify">
-		</div>
-
+	<form class="text-center">
+		<input type="email" name="email" placeholder="Email" class="email" autocomplete="off">
+		<input type="submit" value="Notify Me" class="notify">
 	</form>
 
 </div>
