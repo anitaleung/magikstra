2588556d91111718896cfa8cc30c7b9d976b2649
Anita Leung <anita.wk.leung@gmail.com>
2014-03-15 16:49:12 -0700

    Update sharing links

--- share.php
+++ share.php
@@ -1,9 +1,9 @@
 <div class="row text-center">
 	
 	<ul id="share">
-		<li id="twitter-icon"><a href=""></a></li>
-		<li id="facebook-icon"><a href=""></a></li>
-		<li id="email-icon"><a href=""></a></li>
+		<li id="twitter-icon"><a href="https://twitter.com/Magikstra?utm_source=fb&utm_medium=fb&utm_campaign=Magikstra&utm_content=437403819631464448"></a></li>
+		<li id="facebook-icon"><a href="https://www.facebook.com/pages/Magikstra/217156498482062"></a></li>
+		<li id="email-icon"><a href="mailto:support@magikstra.com"></a></li>
 	</ul>
 
 </div>
