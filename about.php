fece7dcda3fd8691b49f4422666e2ffc6dd6c67a
heather huynh <amphy@uga.edu>
2014-03-15 11:24:19 -0400

    Update about
    all should be fixed

--- about.php
+++ about.php
@@ -1,39 +1,47 @@
 <div class="row">
-	<div class="large-6 columns">
-	<center><h1 class="about"><b>ABOUT US</b></h1></center>
+	<div class="large-12 columns text-center">
+	<h1 id="about" class="section-title">About Us</h1>
 	</div>
 </div>
 <br />
 <div class="row">
-	<div class="large-6 columns">
-	<img src="/img/circle1.gif">
+	<div class="large-4 columns">
+	<img src="img/circle1.gif">
 	</div>
-	<div class="large-6 columns">
-	<h2 class="first">Design</h2><p></p>
+	<div id="about-words" class="large-8 columns">
+	<h2 class="first">Design</h2>
+	<p id="about-description">
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	</p>
 	</div>
 </div>
 <br />
 <div class="row">
-	<div class="large-6 columns">
-	<h2 class="second">Usability</h2><p></p>
+	<div id="about-words" class="large-8 columns">
+	<h2 class="second">Usability</h2>
+	<p id="about-description">
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	</p>
 	</div>
-	<div class="large-6 columns">
-	<img src="/img/circle2.gif">
+	<div class="large-4 columns">
+	<img src="img/circle2.gif">
 	</div>
 </div>
 <br />
 <div class="row">
-	<div class="large-6 columns">
-	<img src="/img/circle3.gif">
+	<div class="large-4 columns">
+	<img src="img/circle3.gif">
 	</div>
-	<div class="large-6 columns">
-	<h2 class="third">Experience<h2><p></p>
+	<div id="about-words" class="large-8 columns">
+	<h2 class="third">Experience</h2>
+	<p id="about-description">
 	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	</p>
 	</div>
 </div>
 
 
 </div>
+
+
 
