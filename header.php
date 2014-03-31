9cc539177def22ff1a4a8983dd1a6922b573b071
Anita Leung <anita.wk.leung@gmail.com>
2014-03-30 20:47:11 -0700

    Fixed roles and names

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#">
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -6,6 +7,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta property="og:image" content ="img/preview.png">
         <title>Magikstra - A networking site for students to connect with mentors and university faculty members.</title>
         <meta name="description" content="A networking site for students to connect with mentors and university faculty members.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,9 +16,7 @@
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-       <!-- <link rel="shortcut icon" href="/favicon.ico" type="image/icon"> <link rel="icon" href="/favicon.ico" type="image/icon"> -->
-
-        <link rel="shortcut icon" type="image/png" href="/favicon.png"/> <link rel="icon" href="/favicon.png" type="image/png">
+        <link rel="icon" type="image/ico" href="/favicon.ico"/>
         
         <!-- MailChimp -->
         <link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
@@ -28,7 +28,6 @@
 
     </head>
     <body>
-
     <div id="fb-root"></div>
     <script>(function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
